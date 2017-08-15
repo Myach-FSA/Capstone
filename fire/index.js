f90c10d89bb45395dfdb6155c4a15ba0ac346f57
DenisUAL <denisandreev64@gmail.com>
2017-08-15 16:26:53 -0400

    position of spheres updates in firebase

--- fire/index.js
+++ fire/index.js
@@ -2,12 +2,12 @@ const firebase = require('firebase')
 
 // -- // -- // -- // Firebase Config // -- // -- // -- //
 const config = {
-  apiKey: "AIzaSyBYhxMkpdLLv3Ze187wNleejBtDTOAJvUA",
+  apiKey: "AIzaSyD5CuKXBrzpUH_m8ovpCdXCNbQ5uRKkPmI",
   authDomain: "myachnv.firebaseapp.com",
-  databaseURL: "https://myachnv.firebaseio.com",
+  databaseURL: "https://myach-fsa.firebaseio.com/",
   projectId: "myach-fsa",
   storageBucket: "",
-  messagingSenderId: "149250633671"
+  messagingSenderId: "51959276382"
 }
 // -- // -- // -- // -- // -- // -- // -- // -- // -- //
 
