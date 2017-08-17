58451b4e12e33257bc00b3dbb792d0c8e2a94a43
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-17 19:55:57 -0400

    Need Fix: State does not update properly

--- app/components/ChooseBall.jsx
+++ app/components/ChooseBall.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import { Link } from 'react-router-dom'
+import { Link, NavLink, Router } from 'react-router-dom'
 
 const balls = [
   { name: 'Heavy Duty', description: 'Ball fashioned by the vikings themselves. This is the ball of choice for those serious about strength.', img: './assets/textures/grayball-choose.png' },
@@ -10,53 +10,112 @@ const balls = [
 class ChooseBall extends React.Component {
   constructor(props) {
     super(props)
-    this.gameSetup = this.gameSetup.bind(this)
+      this.gameId = '';
+    this.ballChoice = this.ballChoice.bind(this)
+    this.gameChoice = this.gameChoice.bind(this)
   }
-  
+
   componentDidMount() {
     const user = this.props.loginObj
-    console.log('User', user)
+    console.log('This is the user', user)
     this.props.setUser(user)
   }
 
-  gameSetup () {
+  shouldComponentUpdate(nextProps){
+    const differentGameRoomId = this.props.user.gameId !== nextProps.gameId;
+    console.log('diff', differentGameRoomId)
+    return differentGameRoomId
+  }
 
+  ballChoice(evt) {
+    this.props.chooseBall(+evt.target.id)
+  }
+
+  gameChoice() {
+    const num = (Math.floor(Math.random() * 90000) + 10000).toString();
+    this.gameId = num;
+    this.props.chooseGame(num)
+    document.getElementById('gameID').value = num;
   }
 
   render() {
-
+    console.log('is the user here? ', this.props.user.username)
     const playerName = this.props.user.username ? this.props.user.username : 'Anonymous'
-
+    
+    const gameID = this.gameId && this.gameId
+    console.log('This is the gameID', gameID)
 
     return (
-      <div className="content has-text-centered">
-        <h1>Hi <strong>{ playerName }</strong>! Choose Your Ball</h1>
-        <div className="horiz-marg">
-          <div className="columns is-multiline">
-            {balls && balls.map((ball, i) => (
-              <article key={i}
-                className="column is-one-third product-grid-item">
-                <div key={ball.id} className="inner-product">
-                  <br />
-                  <figure className="image">
-                    <img src={ball.img} alt="Image" />
-                  </figure>
-                  <p className="subtitle">{ball.name}</p>
-                  <p className="subtitle">{ball.description}</p>
-                  <button onClick={(evt) => this.ballChoice(i)}
-                    className="button is-success is-outlined playnow">
-                    Choose Ball
-                  </button>
-                </div>
-              </article>
-            )
-            )}
+      <div className="container is-fluid">
+        <div className="content has-text-centered">
+          <h1>Hi <strong>{playerName}</strong></h1>
+          <div className="notification">
+            <h3>Choose Your Ball</h3>
+            <i className="fa fa-arrow-down fa-lg" aria-hidden="true"></i>
+            <div className="horiz-marg">
+              <div className="columns is-multiline">
+                {balls && balls.map((ball, i) => (
+                  <article key={i}
+                    className="column is-one-third product-grid-item">
+                    <div key={ball.id} className="inner-product">
+                      <br />
+                      <figure className="image">
+                        <img src={ball.img} alt="Image" />
+                      </figure>
+                      <p className="subtitle">{ball.name}</p>
+                      <p className="subtitle">{ball.description}</p>
+                      <button id={i} onClick={(evt) => this.ballChoice(evt)}
+                        className="button is-success is-outlined playnow">
+                        Choose Ball
+                      </button>
+                    </div>
+                  </article>
+                )
+                )}
+              </div>
+            </div>
           </div>
+          <br></br>
         </div>
+        <div className="content has-text-centered">
+          <div className="notification">
+            <h3>Pick Your Game</h3>
+            <i className="fa fa-arrow-down fa-lg" aria-hidden="true"></i>
+            <br></br>
+            <div className="columns">
+              <div className="column is-half">
+                <h5>Initiate a Game and send the code to your friends</h5>
+                <div className="field has-addons">
+                  <div className="control">
+                    <a className="button is-info" onClick={(evt) => this.gameChoice(evt)}>
+                      Start New Game
+                  </a>
+                  </div>
+                  <div className="control">
+                    <input id="gameID" className="input" type="text" placeholder="Game ID" disabled/>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <h5>Enter a specific game room ID</h5>
+                <div className="field has-addons">
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Enter ID" />
+                  </div>
+                  <div className="control">
+                    <a className="button is-info">
+                      Enter Game
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Link to={`/game/${gameID}`}>
+            <button className="button is-success">Click Here to Enter Game</button>
+        </Link>
         <br></br>
-        <button type="submit" className="button is-primary is-fullwidth fullButton">
-          <Link to={`/game`}>Play Now!</Link>
-        </button>
+        </div>
       </div>
     );
   }
@@ -64,14 +123,14 @@ class ChooseBall extends React.Component {
 
 // /* -----------------    CONTAINER     ------------------ */
 
-import { setUser } from '../reducers/auth'
+import { setUser, chooseBall, chooseGame } from '../reducers/auth'
 import { connect } from 'react-redux'
 import store from '../store';
 
 const mapStateToProps = (state) => ({
-  user: state.auth.user 
+  user: state.auth.user
 })
 
-const mapDispatch = ({ setUser })
+const mapDispatch = ({ setUser, chooseBall, chooseGame })
 
 export default connect(mapStateToProps, mapDispatch)(ChooseBall)
