7c73786d12cb5e13222f2e56b69e6f4b56871573
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-18 13:33:26 -0400

    bs commit

--- app/components/Scene1.jsx
+++ app/components/Scene1.jsx
@@ -12,7 +12,7 @@ const createScene1 = (canvas, engine) => {
   const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
   light.intensity = 0.7 // default is 1, so this is slightly dimmed
 
-  var background = new BABYLON.Layer("back", "./assets/textures/fractal.jpg", scene);
+  var background = new BABYLON.Layer("back", "./assets/textures/green_black_cubes.jpg", scene);
 	background.isBackground = true;
 	background.texture.level = 0;
 	background.texture.wAng = .2;
@@ -46,9 +46,9 @@ const createScene1 = (canvas, engine) => {
   var groundMaterial = new BABYLON.StandardMaterial('material', scene)
   // var textureGrass = new BABYLON.Texture('./assets/textures/chuttersnap.jpg', scene)
   // groundMaterial.diffuseTexture = textureGrass
-  const groundColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+  const groundColor = new BABYLON.Color3(0, 1, 0);
   groundColor.hasAlpha = true;
-  groundMaterial.alpha = 0.8;
+  groundMaterial.alpha = 0.6;
   groundMaterial.diffuseColor = groundColor;
   ground.material = groundMaterial;
 
