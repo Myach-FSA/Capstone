728c9db78acd0ceca0fffc4e242f5d0ff1f002ad
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-17 21:51:28 -0400

    Fix: can now route to game waiting room

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,7 +4,7 @@ import ReactDOM, { render } from 'react-dom'
 const Home = () => (
     <section className="hero">
     <div className="slider">
-    <img id="photoobj" className="media-object" src='https://images.unsplash.com/photo-1464621922360-27f3bf0eca75?dpr=1&auto=format&fit=crop&w=1080&h=720&q=80&cs=tinysrgb&crop=' />
+    <img id="photoobj" className="media-object" src='assets/textures/blue_walkway_thin.png' />
     </div>
     </section>
   )
