773b05a568aa7fe7d82a45404ab5dce2c53804d3
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-18 10:41:52 -0400

    Fix: Game Wait Room works

--- app/components/GameWaitRoom.jsx
+++ app/components/GameWaitRoom.jsx
@@ -3,14 +3,13 @@ import ReactDOM, { render } from 'react-dom';
 import { Link } from 'react-router-dom'
 
 class GameWaitRoom extends React.Component {
-
   render() {
-
+    let numPlayer = 1;
     return (
         <div className="content has-text-centered">
           <div className="notification">
             <h3>Waiting on Players</h3>
-            <h5>Current number of connected players:</h5>
+            <h5>Current number of connected players: {numPlayer}</h5>
             <a className="button is-info" type="submit" title="playbutton" disabled>Play Now!</a>
               {/* <Link to={`/game`}>Play Now!</Link> */}
           </div>
