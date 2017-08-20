26b991bea2ac5951ac753a0b943ab725b18f4726
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-20 16:35:50 -0400

    style changes

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM, { render } from 'react-dom'
 
 const Home = () => (
-    <section className="hero">
+    <section id="contain" className="hero">
     <div className="slider">
     <img id="photoobj" className="media-object" src='assets/textures/blue_walkway_thin.png' />
+    <div className="button is-success" id="overlay"><a href="#"> BUTTON </a></div>
     </div>
     </section>
   )
