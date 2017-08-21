1ca94b4745ec651e8d73ccc71b65daff9229853a
Yu Lin <yu@Yus-MacBook-Pro.fios-router.home>
2017-08-20 21:19:37 -0400

    feat: added public/private button

--- app/components/PrivateGameRoom.jsx
+++ app/components/PrivateGameRoom.jsx
@@ -11,25 +11,31 @@ class GameWaitRoom extends React.Component {
     super();
     this.state = {
       numberOfPlayers: 0,
+      publicGame: false,
     };
   }
   componentDidMount() {
     const database = firebase.database();
     const user = this.props.user;
-    firebase.database().ref('games').update({[user.gameId]: {playersInGame: [user.userId]}});
+    firebase.database().ref('games').update({ [user.gameId]: { playersInGame: [user.userId] } });
     this.getPlayers(user.gameId);
   }
 
   getPlayers = (gameId) => {
     firebase.database().ref('games').on('value', players => {
-      this.setState({numberOfPlayers: players.val()[gameId].playersInGame.length});
+      this.setState({ numberOfPlayers: players.val()[gameId].playersInGame.length });
     });
   }
+
+  security = () => {
+    this.setState({ publicGame: !this.state.publicGame });
+  }
+
   // Will use sendInfo later for scene selection / public or private games
   sendInfo = (info) => {
     const database = firebase.database();
     const user = this.props.user;
-    firebase.database().ref('games').update({[user.gameId]: {playersInGame: [user.userId]}});
+    firebase.database().ref('games/' + user.gameId).update({ security: this.state.security });
   }
 
   render() {
@@ -41,8 +47,8 @@ class GameWaitRoom extends React.Component {
             <h1><strong>Your Game ID: {this.props.user.gameId}</strong></h1>
             <p>Send this code to your friends!</p>
           </div>
-            <SceneList />
-            <ChooseBall />
+          <SceneList />
+          <ChooseBall />
           <h5 id="greenText">Current number of connected players: {this.state.numberOfPlayers}</h5>
           <Link to={`/game`}>
             <button
@@ -53,6 +59,18 @@ class GameWaitRoom extends React.Component {
               Play Now!
             </button>
           </Link>
+          <a
+            className="button is-success"
+            type="submit"
+            title="publicPrivate"
+            onClick={() => { this.security(); }}>
+            {this.state.publicGame &&
+            <i className="fa fa-unlock"> Public</i>
+            }
+            {!this.state.publicGame &&
+            <i className="fa fa-lock"> Private</i>
+            }
+          </a>
         </div>
         <div>
           <h4></h4>
