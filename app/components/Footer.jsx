882345f8a86295f45c32d12cffa55e51a52239d4
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-23 12:02:03 -0400

    changed table colors

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,15 +4,13 @@ import { NavLink } from 'react-router-dom';
 const FooterSection = () => {
   return (
     <footer id="footersec">
-      <div className="container">
-        <div className="has-text-centered">
-          <p>By Dan, Denis, Yu, and Nancy</p>
+      <div className="container has-text-centered">
+        <p>By Dan, Denis, Yu, and Nancy</p>
           <p>
             <a className="icon" href="https://github.com/Myach-FSA/Capstone">
               <i className="fa fa-github"></i>
             </a>
           </p>
-        </div>
       </div>
     </footer>
   );
