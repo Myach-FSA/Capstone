0b635b1a6d25af8ff48aad607caccdd2f8333078
Yu Lin <yu@Yus-MacBook-Pro.fios-router.home>
2017-08-27 02:52:01 -0400

    style: css homepage images centered, head title  changed

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,15 +3,17 @@ import ReactDOM, { render } from 'react-dom';
 import PlayNow from './PlayNow';
 
 const Home = () => (
-    <section id="contain" className="hero">
-    <PlayNow />
+  <section id="contain" className="hero">
+    <div className='homeContainer'>
+      <PlayNow />
+    </div>
     <div className='notification homeScreen'>
-      <img src="/assets/howtoplay.png"/>
+      <img id='tutorial' src="/assets/howtoplay.png" />
     </div>
     <div className="slider">
-    <img id="photoobj" className="media-object" src='assets/textures/blue_walkway_thin.png' />
+      <img id="photoobj" className="media-object" src='assets/textures/blue_walkway_thin.png' />
     </div>
-    </section>
-  );
+  </section>
+);
 
 export default Home;
