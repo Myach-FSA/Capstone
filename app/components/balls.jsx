33427d15d5c4fe53722f003fc7c55d63b04ef098
nancyvelasquez <nevelasquez113@gmail.com>
2017-08-27 12:33:29 -0400

    Fix: Home screen and info screen

--- app/components/balls.jsx
+++ app/components/balls.jsx
@@ -1,16 +1,16 @@
 const balls = [
-    { name: 'White Shapes', description: 'You are an artist.', img: '/assets/textures/students/3dsquares.png' },        
-    { name: 'Flowers on Glass', description: 'Fearsome flowers', img: '/assets/textures/students/floweryglass.png' },
-    { name: 'Grass', description: 'Maybe you like grass.', img: '/assets/textures/students/grass-large.png' },
-    { name: 'Heavy Duty', description: 'Ball fashioned by the vikings themselves.', img: '/assets/textures/students/stone.png' },
-    { name: 'Wood', description: 'Fine oak', img: '/assets/textures/students/andrew-collins-1616.png' },    
-    { name: 'Dreams', description: 'Ball for wanderers.', img: '/assets/textures/students/billy-huynh-278252.png' },    
-    { name: 'Life', description: 'You like things that move with the grace of a cheetah.', img: '/assets/textures/students/joel-filipe-200538.png' },    
-    { name: 'Dreams', description: 'You like things that move with the grace of a cheetah.', img: '/assets/textures/students/lyndsey-b-35202.png' },  
-    { name: 'Sleuth', description: 'You like things that move with the grace of a cheetah.', img: '/assets/textures/students/net.png' },  
-    { name: 'Universe', description: 'You like things that move with the grace of a cheetah.', img: '/assets/textures/students/octavian-rosca-208933.png' },   
-    { name: 'Night', description: 'You are a dangerous mystery.', img: '/assets/textures/students/paul-volkmer-135317.png' },    
-    { name: 'Vision', description: 'You like things that move with the grace of a cheetah.', img: '/assets/textures/students/silvio-kundt-65518.png' },    
+    { name: 'White Shapes', img: '/assets/textures/students/3dsquares.png' },        
+    { name: 'Flowers on Glass', img: '/assets/textures/students/floweryglass.png' },
+    { name: 'Grass', img: '/assets/textures/students/grass-large.png' },
+    { name: 'Heavy Duty', img: '/assets/textures/students/stone.png' },
+    { name: 'Wood', img: '/assets/textures/students/andrew-collins-1616.png' },    
+    { name: 'Dreams', img: '/assets/textures/students/billy-huynh-278252.png' },    
+    { name: 'Life', img: '/assets/textures/students/joel-filipe-200538.png' },    
+    { name: 'Dreams', img: '/assets/textures/students/lyndsey-b-35202.png' },  
+    { name: 'Sleuth', img: '/assets/textures/students/net.png' },  
+    { name: 'Universe', img: '/assets/textures/students/octavian-rosca-208933.png' },   
+    { name: 'Night', img: '/assets/textures/students/paul-volkmer-135317.png' },    
+    { name: 'Vision', img: '/assets/textures/students/silvio-kundt-65518.png' },    
   ];
 
   export default balls;
