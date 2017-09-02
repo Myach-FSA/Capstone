15c3b4e3e4d9aeabe1d5bec90f21d766f7e64581
Yu Lin <yulin89@gmail.com>
2017-09-02 17:33:20 -0400

    Optimize: Convert images from png to webp format at quality of 80

--- app/components/balls.jsx
+++ app/components/balls.jsx
@@ -13,4 +13,19 @@ const balls = [
     { name: 'Vision', img: '/assets/textures/students/silvio-kundt-65518.png' },
 ];
 
+const ballImages= [
+  { name: 'White Shapes', img: '/assets/textures/students/3dsquares.webp' },
+    { name: 'Flowers on Glass', img: '/assets/textures/students/floweryglass.webp' },
+    { name: 'Grass', img: '/assets/textures/students/grass-large.webp' },
+    { name: 'Heavy Duty', img: '/assets/textures/students/stone.webp' },
+    { name: 'Wood', img: '/assets/textures/students/andrew-collins-1616.webp' },
+    { name: 'Dreams', img: '/assets/textures/students/billy-huynh-278252.webp' },
+    { name: 'Life', img: '/assets/textures/students/joel-filipe-200538.webp' },
+    { name: 'Dreams', img: '/assets/textures/students/lyndsey-b-35202.webp' },
+    { name: 'Sleuth', img: '/assets/textures/students/net.webp' },
+    { name: 'Universe', img: '/assets/textures/students/octavian-rosca-208933.webp' },
+    { name: 'Night', img: '/assets/textures/students/paul-volkmer-135317.webp' },
+    { name: 'Vision', img: '/assets/textures/students/silvio-kundt-65518.webp' },
+];
+
 export default balls;
