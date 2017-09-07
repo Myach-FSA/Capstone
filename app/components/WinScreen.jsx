512e52b627fdbb44cd8a7dc19c0396414cb91138
Yu Lin <yulin89@gmail.com>
2017-09-06 23:55:25 -0400

    Fix: Rewrote component to stop update during an existing state transition.

--- app/components/WinScreen.jsx
+++ app/components/WinScreen.jsx
@@ -1,56 +1,56 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import ReactDOM, {render} from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 
 class WinScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      component: null
+      hasWinner: null
     };
-    this.loop;
-    this.endGame = this
-      .endGame
-      .bind(this);
   }
 
-  endGame() {
-    setTimeout(() => {
-      this.props.history.push('/');
-    }, 5000);
+  componentWillMount() {
+    const gameId = this.props.user.gameId;
+    const user = this.props.user.userId;
+    this.props.database.ref('games/' + gameId + '/gameInfo').on('value', victor => {
+      if (victor.val().winner === user) {
+        this.setState({hasWinner: <img className="winScreen" src="/assets/winScreen.png" />});
+      } else if (victor.val().winner !== user && victor.val().winner) {
+        this.setState({ hasWinner: <img className="winScreen" src="/assets/defeatScreen.png" /> });
+      }
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const gameId = this.props.user.gameId;
+    const user = this.props.user.userId;
+    if (this.props.user.totalScore >= 2 && this.props.user.totalScore !== prevProps.user.totalScore) {
+      this.props.database.ref('games/' + gameId + '/gameInfo').update({ winner: user });
+      this.props.database.ref('users/' + user).update({ totalScore: 0 });
+    }
+  }
+
+  componentWillUnmount() {
+    const gameId = this.props.user.gameId;
+    this.props.database.ref('games/' + gameId + '/gameInfo').off();
+  }
+
+  endGame = () => {
+    if (this.state.hasWinner) {
+      setTimeout(() => {
+        this.props.history.push('/');
+      }, 5000);
+    }
+    return this.state.hasWinner;
   }
 
   render() {
-    this
-      .props
-      .database
-      .ref('event')
-      .on('value', (eventMessage) => {
-        const eventType = eventMessage
-          .val()
-          .split(',')[0];
-        const gameId = eventMessage
-          .val()
-          .split(',')[1];
-        const userId = eventMessage
-          .val()
-          .split(',')[2];
-        if (eventType === 'win' && gameId === this.props.user.gameId && userId === this.props.user.userId && !this.state.component) {
-          this.setState({component: <img className="winScreen" src="/assets/winScreen.png"/>});
-          this.endGame();
-        } else if (eventType === 'win' && gameId === this.props.user.gameId && userId !== this.props.user.userId && !this.state.component) {
-          this.setState({component: <img className="winScreen" src="/assets/defeatScreen.png"/>});
-          this.endGame();
-        }
-      });
-    const gameId = this.props.user.gameId;
-    const user = this.props.user.userId;
-    if (this.props.user.totalScore >= 10) {
-      const eventMessage = 'win,' + gameId + ',' + user;
-      this.props.database.ref('event').set(eventMessage);
-      this.props.database.ref('users/' + user).update({totalScore: 0});
-    }
-    return this.state.component;
+    return (
+      <div>
+        {this.endGame()};
+      </div>
+    );
   }
 };
 
